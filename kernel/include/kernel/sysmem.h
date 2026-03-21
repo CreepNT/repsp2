05241a5c6d40f0d787bbac476d50dfdbafe7c322
CreepNT <creepnt69@gmail.com>
2026-03-21 18:25:54 +0100

    kernel: include: add sysmem megaheader
    Add a unified `sysmem.h` which includes all sysmem-related headers

---
+++ kernel/include/kernel/sysmem.h
@@ -0,0 +1,11 @@
+
+#ifndef _SCE_KERNEL_SYSMEM_H_
+#define _SCE_KERNEL_SYSMEM_H_
+
+#include <kernel/sysmem/oheap.h>
+#include <kernel/sysmem/spinlock.h>
+#include <kernel/sysmem/sysclib.h>
+#include <kernel/sysmem/uid_class.h>
+#include <kernel/sysmem/uid_heap.h>
+
+#endif /* _SCE_KERNEL_SYSMEM_H_ */
